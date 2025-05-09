7ee026aa0d917b44419b3248ac65baf1b8d83623
Molly825 <mollychien825@gmail.com>
2025-05-09 11:32:23 +0800

    Create data - 複製.js

---
+++ home/data - 複製.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "兒童新樂園入口對面",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.8748566278634833,
+        "pitch": -0.03415400720627204,
+        "fov": 0.5868085977423275
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.807239308609427,
+          "pitch": -0.006561067761609252,
+          "title": "兒童新樂園",
+          "text": "臺北市兒童新樂園前身為位於圓山的臺北市立兒童育樂中心，在95年4月11日兒童育樂中心全園區納入國定遺址範圍，因所處地理位置受航高、文化資產保存法、水利法等諸多限制，於原址進行開發、更新及營運等受到相當大的影響及限制。<div><br></div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
